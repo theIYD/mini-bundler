54f080484d521f220e8c11fa1a312bc59f1071d5
Idrees Dargahwala <theiyd@Idreess-MacBook-Pro.local>
2023-11-28 09:57:00 +0530

    test: loadAsDirectory

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,17 +1,17 @@
 import fs from "fs";
 import path from "path";
-import { loadAsFile } from "../src/resolver";
+import { loadAsDirectory, loadAsFile } from "../src/resolver";
 
 describe("TEST - resolver", () => {
-	beforeAll(() => {
+	beforeEach(() => {
 		fs.mkdirSync("./tests/mocks");
 	});
 
-	afterAll(() => {
+	afterEach(() => {
 		fs.rmSync("./tests/mocks", { recursive: true });
 	});
 
-	it("should resolve to a file path", () => {
+	it("should resolve a JS file to path", () => {
 		fs.writeFileSync("./tests/mocks/index.js", "// Mock JS File");
 		const pathOne = path.join(__dirname, "./mocks/index");
 		const resolvedPathObject = loadAsFile(pathOne);
@@ -19,10 +19,31 @@ describe("TEST - resolver", () => {
 		expect(resolvedPathObject.relativePath).toContain(".js");
 	});
 
-	it("should NOT resolve to a file path", () => {
+	it("should NOT resolve a non-JS file to a path", () => {
 		fs.writeFileSync("./tests/mocks/index2.txt", "// Mock TXT File");
 		const pathOne = path.join(__dirname, "./mocks/index2");
 		const resolvedPathObject = loadAsFile(pathOne);
+		expect(resolvedPathObject.isFile).toBe(false);
+	});
+
+	it("should NOT resolve a directory to a path", () => {
+		const pathOne = path.join(__dirname, "./mocks/");
+		const resolvedPathObject = loadAsFile(pathOne);
+		expect(resolvedPathObject.isFile).toBe(false);
+	});
+
+	it("should resolve to a directory", () => {
+		fs.writeFileSync("./tests/mocks/index.js", "// Mock JS File");
+		const pathOne = path.join(__dirname, "./mocks/");
+		const resolvedPathObject = loadAsDirectory(pathOne);
+		expect(resolvedPathObject.isFile).toBe(true);
+		expect(resolvedPathObject.relativePath).toContain(".js");
+	});
+
+	it("should NOT resolve a non-JS file to a directory", () => {
+		fs.writeFileSync("./tests/mocks/index2.txt", "// Mock TXT File");
+		const pathOne = path.join(__dirname, "./mocks/");
+		const resolvedPathObject = loadAsDirectory(pathOne);
 		expect(resolvedPathObject.isFile).toBe(false);
 	});
 });
